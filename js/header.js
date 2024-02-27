0464c99abbcfd95aab68ef647f7f438dc4df9c22
Lucas Pinheiro Soares <129182642+soaresluccas@users.noreply.github.com>
2024-02-27 17:25:29 -0300

    Update header.js

--- js/header.js
+++ js/header.js
@@ -7,12 +7,12 @@ window.addEventListener('scroll', () => {
   let scrollTop = window.scrollY || window.pageYOffset;
 
   if (scrollTop > lastScrollTop) {
-    
+    // Scroll para baixo
     if (!headerClassList.contains('scrollHide')) {
       headerClassList.add('scrollHide');
     }
   } else {
-    
+    // Scroll para cima
     if (scrollTop < lastScrollTop - 2 || scrollTop <= 0) {
       headerClassList.remove('scrollHide');
     }
@@ -20,3 +20,4 @@ window.addEventListener('scroll', () => {
   
   lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
 });
+
